3b83b04b7a0e58d06d21c584d8d533ce0b3d5e2d
Ramiel2009 <ramiel2009@gmail.com>
2014-09-08 17:02:28 +0300

    case switch solution

--- Sep2014/src/ua/my/homework/HomeWork1_1.java
+++ Sep2014/src/ua/my/homework/HomeWork1_1.java
@@ -8,10 +8,37 @@ package ua.my.homework;
 public class HomeWork1_1 {
 
 	public static void main(String[] args) {
-int sum = 400;
+int sum = 1000;
+int id = sum/100;
+switch (id) {
+	case  0:
+		System.out.println("Sum = "+sum+". Discount = 0%. Total = "+sum);
+break;
+	case  1:
+		System.out.println("Sum = "+sum+". Discount = 0%. Total = "+sum);
+		break;
+	case  2:
+		System.out.println("Sum = "+sum+". Discount = 0%. Total = "+sum);
+		break;
+	case  3:
+		System.out.println("Sum = "+sum+". Discount = 3%. Total = "+sum + "-3% = "+(sum-(sum/100*3)));
+		break;
+	case  4:
+		System.out.println("Sum = "+sum+". Discount = 3%. Total = "+sum + "-3% = "+(sum-(sum/100*3)));
+		break;
+	case  5:
+		System.out.println("Sum = "+sum+". Discount = 5%. Total = "+sum + "-5% = "+(sum-(sum/100*5)));
+		break;
+	case 6:
+		System.out.println("Sum = "+sum+". Discount = 5%. Total = "+sum + "-5% = "+(sum-(sum/100*5)));
+		break;
+	case 7:
+		System.out.println("Sum = "+sum+". Discount = 5%. Total = "+sum + "-5% = "+(sum-(sum/100*5)));
+		break;
+	default:
+		System.out.println("Sum = "+sum+". Discount = 10%. Total = "+sum + "-10% = "+(sum-(sum/100*10)));
+		break;
 
-switch
-
-	}
+}
 	}
 }
